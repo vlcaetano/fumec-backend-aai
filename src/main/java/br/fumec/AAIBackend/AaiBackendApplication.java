d67e8627b899014fe9919c86c23cac57f462fca5
Vitor Caetano <vitorlc95@gmail.com>
2021-06-06 13:41:53 -0300

    Project created

---
+++ src/main/java/br/fumec/AAIBackend/AaiBackendApplication.java
@@ -0,0 +1,13 @@
+package br.fumec.AAIBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AaiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fumec/AAIBackend/AaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fumec.AAIBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
